a9e6e07169b8a83f021afe480c07e00adf37e408
Dandan Li <sdldd520@163.com>
2018-12-26 10:41:25 +0800

    runme

--- hardnet/code/run_me.sh
+++ hardnet/code/run_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ..
-python ./code/HardNet.py --training-set=notredame --gpu-id=2 --lr=10.0 --fliprot=False --experiment-name=/notredame_train/ | tee -a log_HardNet_Lib_notredame.log
+#cd ..
+python HardNet.py --training-set=notredame --gpu-id=0 --lr=10.0 --fliprot=False --experiment-name=../../notredame_train/ | tee -a ../logs/log_HardNet_Lib_notredame.log
